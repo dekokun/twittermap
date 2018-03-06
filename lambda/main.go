4593b8118786773f1032e9b66c348b7bee27320e
dekokun <dekokun@hatena.ne.jp>
2018-03-07 08:54:39 +0900

    s3にuploadする

--- lambda/main.go
+++ lambda/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
@@ -38,7 +43,21 @@ func handleRequest(request events.CloudWatchEvent) (events.CloudWatchEvent, erro
 	spew.Dump(tweets[0].Text)
 	spew.Dump(tweets[0].Entities.Media[0].MediaURLHttps)
 	spew.Dump(tweets[0].Entities.Media[0].ExpandedURL)
+	sess := session.New(&aws.Config{})
+	s3Svc := s3.New(sess, aws.NewConfig().WithRegion("ap-northeast-1"))
+	uploader := s3manager.NewUploaderWithClient(s3Svc)
+	bucket := "twittermap.dekokun.info"
+	key := "hogehogeeeeetests"
+	upParams := &s3manager.UploadInput{
+		Bucket: &bucket,
+		Key:    &key,
+		ACL:    aws.String("public-read"),
+		Body:   strings.NewReader("fugafuga"),
+	}
 
+	result, err := uploader.Upload(upParams)
+	log.Println(result)
+	log.Println(err)
 	return request, nil
 }
 
